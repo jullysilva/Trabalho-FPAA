f2b9323bd9f9be8e4907fae1231e33051ed97504
Jully Ketely <42702863+jullysilva@users.noreply.github.com>
2023-03-15 17:37:09 -0300

    Update main.java

--- main.java
+++ main.java
@@ -1,56 +1,71 @@
 import java.util.Random;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Aplicacao {
+    public static void main(String[] args) {
+    	int iteracoes = 0;
+    	
+    	
+    	while(iteracoes < 50) {
+        ArrayList<Integer> vetor = new ArrayList<Integer>();
+        Random gerador = new Random();
+        
+        for(int i=1; i<=100000; i++){
+            vetor.add(gerador.nextInt());
+        }
+        
+        Collections.shuffle(vetor);
+        
+        // Agora está o vetor original está sendo clonado
+        ArrayList<Integer> copiaVetor = (ArrayList<Integer>) vetor.clone();
+        
+        int n = vetor.size();
+        long comp = 0;
+        long trocas = 0;
+        
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n-i-1; j++) {
+                comp++;
+                if (vetor.get(j) > vetor.get(j+1)) {
+                    trocas++;
+                    int temp = vetor.get(j);
+                    vetor.set(j, vetor.get(j+1));
+                    vetor.set(j+1, temp);
+                }
+            }
+        }
+        
+        System.out.println("RESULTADO BOLHA:\nComparacoes: " + comp + " Trocas: " + trocas);
+        
+        int size = copiaVetor.size();
+        long comparacoes = 0;
+        long count = 0;
 
-	public static void main(String[] args) throws InterruptedException {
-		long startTime = System.currentTimeMillis();
-		
-		
-		int vetor[] = new int[100000];
-		int aux, comparacao = 0, realizada = 0;
-		Random gerador = new Random();
-		
-		for(int i = 0; i < 100000; i++) {
-			vetor[i] = gerador.nextInt();
-		}
-		
-		// ALGOTIMO DE ORDENAÇÃO USANDO O MÉTODO BOLHA DO VETOR ORIGINAL
-		for(int i = 0; i<100000; i++){
-			for(int j = 0; j<4; j++){
-				realizada++;
-				if(vetor[j] > vetor[j + 1]){
-					comparacao++;
-					aux = vetor[j];
-					vetor[j] = vetor[j+1];
-					vetor[j+1] = aux;
-				}
-			}
-		}
-		
-		System.out.println("Realizada: " + realizada + "\nComparada: " + comparacao);
-		
-		int [] vetorB = vetor.clone();
-		
-		// ALGORITMO DE ORDANAÇÃO USANDO O MÉTODO DE SELEÇÃO DO VETOR B CLONADO
-		for (int fixo = 0; fixo < vetorB.length - 1; fixo++) {
-		    int menor = fixo;
+        for (int i = 0; i < size-1; i++) {
+            int minIndex = i;
+            for (int j = i+1; j < size; j++) {
+                comparacoes++;
+                if (copiaVetor.get(j) < copiaVetor.get(minIndex)) {
+                    minIndex = j;
+                }
+            }
+            if (minIndex != i) {
+                count++;
+                int temp = copiaVetor.get(i);
+                copiaVetor.set(i, copiaVetor.get(minIndex));
+                copiaVetor.set(minIndex, temp);
+            }
+        } 
+        
+        System.out.println("RESULTADO SELECAO:\nComparacoes: " + comparacoes + " Trocas feitas: " + count);
+        vetor.clear();
+        copiaVetor.clear();
+        iteracoes++;
+        System.out.println("\n-----------------------------------------------------------------------\n");
+    }
 
-		    for (int i = menor + 1; i < vetorB.length; i++) {
-		       if (vetorB[i] < vetorB[menor]) {
-		          menor = i;
-		       }
-		    }
-		    if (menor != fixo) {
-		      int t = vetorB[fixo];
-		      vetorB[fixo] = vetorB[menor];
-		      vetorB[menor] = t;
-		    }
-		  }
-		
-		long endTime = System.currentTimeMillis();
-		long timeElapsed = endTime - startTime;
-
-		System.out.println("Execution time in milliseconds: " + timeElapsed);
-	}
-
+    	System.out.println(iteracoes);
+    }
 }
